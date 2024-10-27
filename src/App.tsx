4da1dd044e2dd7127c358df30b7490f78cac79e6
Laura Ogbechie <ogbechielaura04@gmail.com>
2024-10-27 14:14:18 +0100

    cleared unused imports in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,3 @@
-import NavBar from "./components/NavBar";
-import FeaturesSection from "./sections/FeaturesSection";
-import IncomeReportSection from "./sections/IncomeReportSection";
-import SalaryPaymentsSection from "./sections/SalaryPaymentsSection";
-import HowItWorksSection from "./sections/HowItWorksSection";
-import PlansSection from "./sections/PlansSection";
-import FaqSection from "./sections/FaqSection";
-import HeroSection from "./sections/HeroSection";
-import TakeControlSection from "./sections/TakeControlSection";
-import Footer from "./components/Footer";
-import TrustedSection from "./sections/TrustedSection";
 import LandingPage from "./pages/LandingPage";
 
 function App() {
